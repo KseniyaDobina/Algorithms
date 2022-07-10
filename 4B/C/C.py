f342cc981fa65e4646b1fa387ccc07c90e30546c
KseniyaDobina <108687454+KseniyaDobina@users.noreply.github.com>
2022-07-10 18:29:08 +0300

    Create C.py

---
+++ 4B/C/C.py
@@ -0,0 +1,16 @@
+with open('input.txt', 'r') as file:
+    words_dict = {}
+    for i in file:
+        for j in i.split():
+            if j in words_dict.keys():
+                new_val = words_dict.get(j)
+                words_dict[j] = new_val + 1
+            else:
+                words_dict[j] = 1
+
+list_word = [(words_dict[i], i) for i in words_dict.keys()]
+
+list_word = sorted(list_word)
+
+for i in sorted(list_word, key=lambda x: x[0], reverse=True):
+    print(i[1])
